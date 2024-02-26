40c24f25bcb0227c39ea539feae6cad729b8b33c
Jakub Budzich <115219579+budziun@users.noreply.github.com>
2024-02-26 11:01:03 +0100

    Add files via upload

---
+++ lab1/klocki.pl
@@ -0,0 +1,25 @@
+% Program klocki_1
+% Baza wiedzy o układzie klocków
+% Definiowane predykaty:
+%       na/2 
+%===============================
+
+% na (X,Y)
+% opis: spełniony,gdy klocek X leży bezpośrednio na klocku Y
+% ----------------------------------------na/2
+	na(c,a).
+	na(c,b).
+	na(d,c).
+%-----------------------------------------na/2
+/*
+Informacje o budowie programu:
+Program składa się z 3 klazul.
+Program zawiera 1 definicje relacji.
+Jest to relacja na/2.
+Definicja relacji na/2 składa się z 3
+klauzul, które są faktami.
+*/
+% otwieranie tego 
+/*
+
+*/
