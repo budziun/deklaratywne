8ffa9fe0a446c07cb354d945e136ceb687375e15
Jakub Budzich <115219579+budziun@users.noreply.github.com>
2024-03-13 14:42:20 +0100

    Add files via upload

--- lab4/rodzic.pl
+++ lab4/rodzic.pl
@@ -4,7 +4,10 @@ rodzic(piotr,ewa).
 przodek(X,Y):-rodzic(X,Y).
 przodek(X,Y):-rodzic(X,Z),przodek(Z,Y).
 /* odp
-?-przodek(ewa,Y) => Y=ania
-?-przodek(piotr,Y) => Y=ewa; Y=ania
-?-przodek(ania,Y) => false.
+?-przodek(piotr,ania). => true
+?-przodek(ania,ewa). => false
+?-przodek(X,ania). => X=ewa;X=piotr
+?-przodek(piotr,Y). => Y=ewa; Y=ania
+?-przodek(ewa,_). => true
+?-przodek(ania,_). => false
 */
